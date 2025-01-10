3110fc5a0551f075b83c42e9c37b5f32d8fba4b1
Saloni <saloniisingh93@gmail.com>
2025-01-10 11:54:15 +0530

    Adding Models & routes Files

---
+++ backend/models/Notes.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose'); // to manage database structure automatically---
+
+
+//create schems to store user data----
+const NotesSchema = new mongoose.Schema({
+title:{
+    type:String,
+    required:true
+},
+description:{
+    type:String,
+    required:true
+},
+tag:{
+    type:String,
+    default:"General"
+},
+date:{
+    type:Date,
+    default:Date.now
+}
+}) ;
+module.exports = mongoose.model('Notes',NotesSchema);//create a schema  giving("modelName","schema")
---
+++ backend/models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose'); // to manage database structure automatically---
+
+
+//create schems to store user data----
+const userSchema = new mongoose.Schema({
+name:{
+    type:String,
+    required:true
+},
+email:{
+    type:String,
+    required:true,
+    unique:true
+},
+password:{
+    type:String,
+    required:true
+},
+date:{
+    type:Date,
+    default:Date.now
+}
+}) ;
+const User = mongoose.model('user',userSchema);
+User.createIndexes();
+module.exports = User  //create a schema  giving("modelName","schema")
