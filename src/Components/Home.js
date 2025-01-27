80f705b634f965c82dcf30c8d557434e612bc522
Saloni <saloniisingh93@gmail.com>
2025-01-27 19:42:00 +0530

    Added react setup

---
+++ src/Components/Home.js
@@ -0,0 +1,22 @@
+//rafc -react Arrow Function Component------
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div>This is Home Page</div>
+    
+  )
+}
+
+
+
+//rfce --React Function import Component
+// import React from 'react'
+
+// function Home() {
+//   return (
+//     <div>Home</div>
+//   )
+// }
+
+// export default Home
